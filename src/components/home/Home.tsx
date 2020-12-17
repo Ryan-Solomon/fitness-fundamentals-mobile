750c6831a400f68466744c8b6ea2692cc55e8df1
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-17 17:05:10 -0600

    pass props to button styled components

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { StyledButton } from './../../styles/buttons';
 
 const Home = () => {
   return (
     <View>
       <Text>Where would you like to start?</Text>
+      <StyledButton text='Go To Quiz' />
     </View>
   );
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Home from '../components/home/Home';
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Home />
     </View>
   );
 };
